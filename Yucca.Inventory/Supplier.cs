bde4e0f84ef16e8705f0a7362f9bfc8c15125dc5
edvella <edvella@gmail.com>
2023-12-19 15:05:38 +0100

    Remove supplier functionality

--- Yucca.Inventory/Supplier.cs
+++ Yucca.Inventory/Supplier.cs
@@ -2,6 +2,7 @@ namespace Yucca.Inventory;
 
 public class Supplier
 {
+    public string Id { get; set;  }
     public string Name { get; set; }
     public string AddressLine1 { get; set; }
     public string AddressLine2 { get; set; }
--- Yucca.Inventory/SupplierList.cs
+++ Yucca.Inventory/SupplierList.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Yucca.Inventory;
 
 public abstract class SupplierList
 {    
-    public abstract void Save(Supplier supplier);
+    public abstract string Save(Supplier supplier);
 
     public abstract Supplier GetFirst();
 
     public abstract IEnumerable<Supplier> FilterByName(string text);
+
+    public abstract void Remove(string id);
 }
