f16cb8b50e637e438301769c9370868564f0067e
edvella <edvella@gmail.com>
2025-05-17 10:27:24 +0200

    Implement SupplierController with GET and POST endpoints for supplier listing and adding respectively

---
+++ Yucca.WebAPI/Controllers/SupplierController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Yucca.Volatile;
+using Yucca.Inventory;
+
+namespace Yucca.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupplierController : ControllerBase
+    {
+        private static readonly InMemorySupplierList supplierList = new();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Supplier>> GetSuppliers()
+        {
+            // Return all suppliers in the in-memory list
+            return Ok(supplierList.FilterByName(string.Empty));
+        }
+
+        [HttpPost]
+        public ActionResult<Supplier> AddSupplier([FromBody] Supplier supplier)
+        {
+            if (supplier == null)
+            {
+                return BadRequest("Supplier data is required.");
+            }
+
+            supplierList.Save(supplier);
+            return CreatedAtAction(nameof(GetSuppliers), new { id = supplier.Id }, supplier);
+        }
+    }
+}
